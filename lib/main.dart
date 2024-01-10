247d8e11946a0ba4bc9e326f2a1e8e89aee769c3
BarsomNaseif <barsomnasif@gmail.com>
2024-01-10 21:57:50 +0200

    title: 'News App',

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'News8App',
+      title: 'News App',
      debugShowCheckedModeBanner: F,
       theme: ThemeData(
         
